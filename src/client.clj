2f5c94d157c5a7b706f7f4cf427f6d984fca6766
Hubert Iwaniuk <neotyk@kungfoo.pl>
2010-03-10 01:14:27 +0100

    getting status line

--- src/client.clj
+++ src/client.clj
@@ -2,34 +2,54 @@
   
   (:import (com.ning.http.client AsyncHttpClient AsyncHandler
 				 HttpResponseStatus HttpResponseHeaders
-				 HttpResponseBodyPart)))
+				 HttpResponseBodyPart RequestBuilder
+				 RequestType)))
 
 (def ahc (AsyncHttpClient.))
 
-(defn statusReceived [status]
-    (println (str "Status code: " (status :code)))
+(defstruct status :protocol :major :minor :code :text)
+
+(defn statusCallback [status]
+    (println (str "Status code: " @(status :code)))
     :abort)
 
+(defn- defaultStatusCallback [status])
+
+(defn convertMethod [method]
+  "Converts clj method (:get, :put, ...) to Async Client specific.
+   Defaults to :get"
+  (cond
+   (= method :get) RequestType/GET
+   (= method :post) RequestType/POST
+   (= method :put) RequestType/PUT
+   (= method :delete) RequestType/DELETE
+   (= method :head) RequestType/HEAD
+   :default RequestType/GET))
+
+(defn prepareRequest [method #^String url]
+  (let [ahcMethod (convertMethod method)
+	rb (RequestBuilder. ahcMethod)]
+    (.. rb (setUrl url) (build))))
+
 (defn prepareGet [#^String url]
-  (.prepareGet ahc url))
+  (prepareRequest :get url))
 
 ;; How do one make
 ;; (let [abc {:a (lazy)}])
 ;; ?
+;; Answer: delay
 (defn testRead [#^String url]
-  (.execute (prepareGet url)
+  (.executeRequest ahc (prepareGet url)
     (proxy [AsyncHandler] []
       (onStatusReceived [#^HttpResponseStatus response]
-	(let [stat {:protocol
-		    (do
-		      (println "Getting protocol")
-		      (.getProtocolName response))
-		    :major (.getProtocolMajorVersion response)
-		    :minor (.getProtocolMinorVersion response)
-		    :code (.getStatusCode response)
-		    :text (.getStatusText response)}
-	      action (statusReceived stat)]
-	  (if (= :abort stat)
+	(let [stat (struct status
+			   (delay (.getProtocolName response))
+			   (delay (.getProtocolMajorVersion response))
+			   (delay (.getProtocolMinorVersion response))
+			   (delay (.getStatusCode response))
+			   (delay (.getStatusText response)))
+	      action (statusCallback stat)]
+	  (if (= :abort action)
 	    com.ning.http.client.AsyncHandler$STATE/ABORT
 	    com.ning.http.client.AsyncHandler$STATE/CONTINUE)))
       (onHeadersReceived [#^HttpResponseHeaders response]
