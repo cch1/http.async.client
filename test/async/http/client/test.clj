db5112bb0b79db29683c4175328b776ce8699e96
Hubert Iwaniuk <neotyk@kungfoo.pl>
2010-06-27 12:25:54 +0200

    Attached Apache License.

--- test/async/http/client/test.clj
+++ test/async/http/client/test.clj
@@ -1,3 +1,17 @@
+; Copyright 2010 Hubert Iwaniuk
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;   http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
 (ns async.http.client.test
   "Testing of ahc-clj"
   #^{:author "Hubert Iwaniuk <neotyk@kungfoo.pl>"}
@@ -5,6 +19,7 @@
   (:import (org.apache.log4j ConsoleAppender Level Logger PatternLayout)))
 
 (defn once-fixture [f]
+  "Configures Logger before test here are executed, and closes AHC after tests are done."
   (doto (Logger/getRootLogger)
     (.setLevel Level/WARN)
     (.addAppender (ConsoleAppender. (PatternLayout. PatternLayout/TTCC_CONVERSION_PATTERN))))
