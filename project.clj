8969f97d4f4b7ec4851f2e8beb4c649dd65609b4
Hubert Iwaniuk <neotyk@kungfoo.pl>
2010-07-16 09:56:14 +0200

    Updated group of lein-javac.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ahc-clj "0.1.0"
+(defproject ahc-clj "0.1.1"
   :description      "Async Http Client for Clojure"
   :namespaces       [async.http.client]
   :dependencies     [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
@@ -7,7 +7,7 @@
   :dev-dependencies [[swank-clojure "1.2.1"]
 		     [leiningen/lein-swank "1.1.0"]
 		     [autodoc "0.7.1"]
-		     [leiningen/lein-javac "1.0.1"]
+		     [org.clojars.neotyk/lein-javac "1.0.1"]
                      [org.eclipse.jetty/jetty-server "7.1.4.v20100610"]]
   :autodoc {:name "ahc-clj"
             :web-src-dir "http://github.com/neotyk/ahc-clj/blob/"
