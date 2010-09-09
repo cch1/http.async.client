84211828417754b5814b8ca21b8fc5679726e84e
Hubert Iwaniuk <neotyk@kungfoo.pl>
2010-09-09 09:21:22 +0200

    Releasing 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http.async.client "0.2.0-SNAPSHOT"
+(defproject http.async.client "0.2.0"
   :description      "Asynchronous HTTP Client for Clojure"
   :namespaces       [http.async.client]
   :dependencies     [[org.clojure/clojure "1.2.0"]
