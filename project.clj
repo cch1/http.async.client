bf5c3f7f21e31b1abed494a2bdb0790c1c5295d5
Hubert Iwaniuk <neotyk@kungfoo.pl>
2010-09-27 22:43:11 +0200

    Update of project.clj to support new leiningen hooks requirements.

--- project.clj
+++ project.clj
@@ -6,12 +6,13 @@
 		     [com.ning/async-http-client "1.2.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
 		     [autodoc "0.7.1"]
-		     [org.clojars.neotyk/lein-javac "1.0.1"]
+		     [org.clojars.neotyk/lein-javac "1.0.2"]
                      [lein-difftest "1.2.2"]
                      [org.eclipse.jetty/jetty-server "7.1.4.v20100610"]
                      [org.danlarkin/clojure-json "1.1"]
                      [lein-run "1.0.0"]]
   :repository {"snapshots" "http://oss.sonatype.org/content/repositories/snapshots/"}
+  :hooks [leiningen.hooks.javac]
   :autodoc {:name "http.async.client"
             :web-src-dir "http://github.com/neotyk/http.async.client/blob/"
             :web-home "http://neotyk.github.com/http.async.client/autodoc/"
