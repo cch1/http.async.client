78ada1007fdff1633e62ee2ac4f71699daa7d1c1
Carlo Sciolla <carlo.sciolla@gmail.com>
2010-11-08 14:26:43 +0100

    Wrap the code in a with-client call to avoid leaking connections

--- sample.clj
+++ sample.clj
@@ -1,8 +1,9 @@
 (ns sample
   (:require [http.async.client :as c]))
 
-(let [r (c/GET "http://example.com/")]
-  (println "done?" (c/done? r))
-  (c/await r)
-  (println "done?" (c/done? r))
-  (c/string r))
+(with-client
+  (let [r (c/GET "http://example.com/")]
+   (println "done?" (c/done? r))
+   (c/await r)
+   (println "done?" (c/done? r))
+   (c/string r)))
