7af0fd81d2e359f640e0b838b872f001ee59f3a4
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-01-06 10:54:04 +0100

    Proper keyword for repositories, as well as trying out marginalia.

--- project.clj
+++ project.clj
@@ -8,13 +8,14 @@
 		     [org.clojure/clojure-contrib "1.2.0"]
 		     [com.ning/async-http-client "1.5.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-		     [autodoc "0.7.1"]
+		     ;;[autodoc "0.7.1"]
+                     [marginalia "0.2.3"]
                      [org.danlarkin/clojure-json "1.1"]
                      [org.eclipse.jetty/jetty-server "7.1.4.v20100610"]
                      [org.eclipse.jetty/jetty-security "7.1.4.v20100610"]
                      [lein-difftest "1.2.2"]
                      [log4j "1.2.13"]]
-  :repository {"snapshots" "http://oss.sonatype.org/content/repositories/snapshots/"}
+  :repositories {"snapshots" "http://oss.sonatype.org/content/repositories/snapshots/"}
   :autodoc {:name "http.async.client"
             :web-src-dir "http://github.com/neotyk/http.async.client/blob/"
             :web-home "http://neotyk.github.com/http.async.client/autodoc/"
