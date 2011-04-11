518dff56e767fa1a93258038cf7ab87f5f1fff67
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-04-11 09:52:47 +0200

    User-Agent version bump.

--- src/clj/http/async/client/request.clj
+++ src/clj/http/async/client/request.clj
@@ -35,7 +35,7 @@
                     ByteArrayInputStream
                     ByteArrayOutputStream)))
 
-(def *user-agent* "http.async.client/0.2.2")
+(def *user-agent* "http.async.client/0.2.3-dev")
 
 (def *CLIENT* nil)
 
