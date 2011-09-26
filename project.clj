256d36227178d1dab650ac7b3710df8d8dad3599
Adam Wynne <adamwynne@gmail.com>
2011-09-26 15:07:32 +0100

    make it push to clojars for me

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http.async.client "0.3.1"
+(defproject org.clojars.adamwynne/http.async.client "0.3.1"
   :description      "Asynchronous HTTP Client for Clojure"
   :url              "http://neotyk.github.com/http.async.client/"
   :source-path      "src/clj"
