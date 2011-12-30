75ae53663e0c35d3582e5377cd3b9cdcf598ef89
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-12-31 00:37:12 +0100

    Next development cycle.
    v0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http.async.client "0.4.0"
+(defproject http.async.client "0.4.1-SNAPSHOT"
   :name             "http.async.client"
   :description      "Asynchronous HTTP Client for Clojure"
   :url              "http://neotyk.github.com/http.async.client/"
