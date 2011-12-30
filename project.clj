8d41d586b5ba01979fa26523c7bef2e4c9e43a04
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-12-31 00:18:05 +0100

    Release v0.4.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http.async.client "0.4.0-SNAPSHOT"
+(defproject http.async.client "0.4.0"
   :name             "http.async.client"
   :description      "Asynchronous HTTP Client for Clojure"
   :url              "http://neotyk.github.com/http.async.client/"
