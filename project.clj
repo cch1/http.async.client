568061478270d6052d2f1a4172e92ae61f28c3fa
Hubert Iwaniuk <hubert@happyhacking.nl>
2012-02-07 00:58:54 +0100

    v0.4.2-dev
    Next development cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http.async.client "0.4.1"
+(defproject http.async.client "0.4.2-SNAPSHOT"
   :name             "http.async.client"
   :description      "Asynchronous HTTP Client for Clojure"
   :url              "http://neotyk.github.com/http.async.client/"
--- src/clj/http/async/client/request.clj
+++ src/clj/http/async/client/request.clj
@@ -32,7 +32,7 @@
                     ByteArrayInputStream
                     ByteArrayOutputStream)))
 
-(def ^:dynamic *user-agent* "http.async.client/0.4.1")
+(def ^:dynamic *user-agent* "http.async.client/0.4.2-dev")
 
 (def ^:dynamic *CLIENT* nil)
 
