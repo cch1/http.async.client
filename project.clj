24a486bcb4be7908d31ed4572b9e8b496bdd33bd
Hubert Iwaniuk <hubert@happyhacking.nl>
2012-04-27 13:37:23 +0200

    codox upgrade.

--- project.clj
+++ project.clj
@@ -7,14 +7,15 @@
   :javac-options {:deprecation "true"}
   :dependencies     [[org.clojure/clojure "1.3.0"]
 		     [com.ning/async-http-client "1.7.1"]]
-  :dev-dependencies [[codox "0.4.0"]
-                     [org.eclipse.jetty/jetty-server "7.1.4.v20100610"]
+  :plugins [[codox "0.6.1"]]
+  :dev-dependencies [[org.eclipse.jetty/jetty-server "7.1.4.v20100610"]
                      [org.eclipse.jetty/jetty-security "7.1.4.v20100610"]
                      [lein-difftest "1.3.3"  :exclusions [org.clojure/clojure
                                                           org.clojure/clojure-contrib]]
                      [log4j "1.2.13"]
                      [org.slf4j/slf4j-log4j12 "1.6.4"]]
   ;; :repositories {"snapshots" "http://oss.sonatype.org/content/repositories/snapshots/"}
+  :codox {:output-dir "doc"}
   :autodoc {:web-src-dir "http://github.com/neotyk/http.async.client/blob/"
             :web-home "http://neotyk.github.com/http.async.client/autodoc/"
             :copyright "Copyright 2012 Hubert Iwaniuk"})
