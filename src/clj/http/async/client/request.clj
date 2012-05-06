8ea22327e3b713498925e6e2482ef25c578b9da6
Hubert Iwaniuk <hubert@happyhacking.nl>
2012-05-06 23:55:46 +0200

    Release v0.4.5.

--- src/clj/http/async/client/request.clj
+++ src/clj/http/async/client/request.clj
@@ -32,7 +32,7 @@
                     ByteArrayInputStream
                     ByteArrayOutputStream)))
 
-(def ^:dynamic *user-agent* "http.async.client/0.4.5-dev")
+(def ^:dynamic *user-agent* "http.async.client/0.4.5")
 
 (def ^:dynamic *CLIENT* nil)
 
