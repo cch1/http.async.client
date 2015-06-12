94f1551be934b7162ce97e44a5374ca1772303b9
Chris Hapgood <cch1@hapgoods.com>
2015-06-12 16:27:19 -0400

    Release version 0.5.3

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject http.async.client "0.5.3-SNAPSHOT"
+(defproject http.async.client "0.5.3"
   :name             "http.async.client"
   :description      "Asynchronous HTTP Client for Clojure"
   :url              "http://cch1.github.com/http.async.client/"
   :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.ning/async-http-client "1.7.10" :exclusions [org.slf4j/slf4j-api]]]
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.5.1"
   :plugins [[codox "0.8.12"]
             [lein-difftest "2.0.0"]]
-  :profiles {:1.3.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+  :profiles {:1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev {:resource-paths ["test-resources"],
