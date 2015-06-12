bdc28b90f6374cfd8a243718e4e7694ef95646b3
Chris Hapgood <cch1@hapgoods.com>
2015-06-11 23:28:00 -0400

    Overhaul org mode documents for publishing

--- org-publish-project-alist.el
+++ org-publish-project-alist.el
@@ -3,11 +3,20 @@
 
 (setq org-publish-project-alist
       '(("gh-notes"
-	  :base-directory "./org"
+	  :base-directory "."
 	  :base-extension "org"
 	  :publishing-directory "./gh-pages"
 	  :recursive t
 	  :publishing-function org-html-publish-to-html
 	  :headline-levels 4
+	  :auto-sitemap t
+	  :sitemap-title "Sitemap"
+	  :sitemap-filename "sitemap.org"
 	  :auto-preamble t)
-	("gh-pages" :components ("gh-notes"))))
+	("gh-assets"
+	 :base-directory "./org/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	 :publishing-directory "./gh-pages"
+	 :recursive t
+	 :publishing-function org-publish-attachment)
+	("gh-pages" :components ("gh-notes" "gh-assets"))))
