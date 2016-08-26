d1573298118a61950279294f50db8f19cc2f3db8
Chris Hapgood <cch1@hapgoods.com>
2016-08-26 15:25:49 -0400

    Clean up test profile dependency w.r.t. lein ancient

--- project.clj
+++ project.clj
@@ -9,11 +9,11 @@
   :min-lein-version "2.5.1"
   :plugins [[codox "0.8.12"]
             [lein-difftest "2.0.0"]]
-  :profiles {:1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6.0 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7.0 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+  :profiles {:1.3.0 {:dependencies [[org.clojure/clojure "1.3.0" :upgrade? false]]}
+             :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0" :upgrade? false]]}
+             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1" :upgrade? false]]}
+             :1.6.0 {:dependencies [[org.clojure/clojure "1.6.0" :upgrade? false]]}
+             :1.7.0 {:dependencies [[org.clojure/clojure "1.7.0" :upgrade? false]]}
              :dev {:resource-paths ["test-resources"],
                    :dependencies [[org.eclipse.jetty/jetty-server "9.4.0.M1"]
                                   [org.eclipse.jetty/jetty-security "9.4.0.M1"]
@@ -21,7 +21,6 @@
                                   [aleph "0.4.1"]
                                   [log4j "1.2.17"]
                                   [org.slf4j/slf4j-log4j12 "1.7.12" :upgrade? false]]}}
-  ;; :repositories {"snapshots" "http://oss.sonatype.org/content/repositories/snapshots/"}
   :codox {:output-dir "gh-pages/doc"
           :src-dir-uri "http://github.com/cch1/http.async.client/blob/master/"
           :src-linenum-anchor-prefix "L"}
