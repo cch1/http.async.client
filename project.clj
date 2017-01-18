1153b11075b5fe2ae812372c54d6585d42150e11
Chris Hapgood <cch1@hapgoods.com>
2017-01-18 15:56:23 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url              "https://github.com/cch1/http.async.client"
   :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.ning/async-http-client "1.9.39"]
+                 [com.ning/async-http-client "1.9.40"]
                  [org.clojure/tools.logging "0.3.1"]]
   :min-lein-version "2.5.1"
   :plugins [[codox "0.8.12"]
@@ -15,9 +15,9 @@
              :1.6.0 {:dependencies [[org.clojure/clojure "1.6.0" :upgrade? false]]}
              :1.7.0 {:dependencies [[org.clojure/clojure "1.7.0" :upgrade? false]]}
              :dev {:resource-paths ["test-resources"],
-                   :dependencies [[org.eclipse.jetty/jetty-server "9.4.0.M1"]
-                                  [org.eclipse.jetty/jetty-security "9.4.0.M1"]
-                                  [org.eclipse.jetty/jetty-continuation "9.4.0.M1"]
+                   :dependencies [[org.eclipse.jetty/jetty-server "9.4.0.RC3"]
+                                  [org.eclipse.jetty/jetty-security "9.4.0.RC3"]
+                                  [org.eclipse.jetty/jetty-continuation "9.4.0.RC3"]
                                   [aleph "0.4.1"]
                                   [log4j "1.2.17"]
                                   [org.slf4j/slf4j-log4j12 "1.7.12" :upgrade? false]]}}
